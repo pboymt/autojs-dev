e4ec9b5c1e0fde69959011d59328f64fdcb20c80
pboymt <pboymt@foxmail.com>
2018-01-22 19:13:55 +0800

    完善app模块

--- src/auto.ts
+++ src/auto.ts
@@ -55,5 +55,81 @@ declare namespace app {
      * 打开应用的详情页(设置页)。如果找不到该应用，返回false; 否则返回true。
      */
     function openAppSetting(packageName: string): boolean;
-
+    /**
+     * 用其他应用查看文件。文件不存在的情况由查看文件的应用处理。如果找不出可以查看该文件的应用，则抛出ActivityNotException。
+     */
+    function viewFile(path: string): void;
+    /**
+     * 用其他应用编辑文件。文件不存在的情况由编辑文件的应用处理。如果找不出可以编辑该文件的应用，则抛出ActivityNotException。
+     */
+    function editFile(path: string): void;
+    /**
+     * 卸载应用。执行后会会弹出卸载应用的提示框。如果该包名的应用未安装，由应用卸载程序处理，可能弹出"未找到应用"的提示。
+     */
+    function uninstall(packageName: string): void;
+    /**
+     * 用浏览器打开网站url。网站的Url，如果不以"http:// "或"https:// "开头则默认是"http:// "。
+     */
+    function openUrl(url: string): void;
+    /**
+     * 发送邮件的参数，这些选项均是可选的。
+     */
+    interface SendEmailOptions {
+        /**
+         * 收件人的邮件地址。如果有多个收件人，则用字符串数组表示
+         */
+        email?: string | string[];
+        /**
+         * 抄送收件人的邮件地址。如果有多个抄送收件人，则用字符串数组表示
+         */
+        cc?: string | string[];
+        /**
+         * 密送收件人的邮件地址。如果有多个密送收件人，则用字符串数组表示
+         */
+        bcc?: string | string[];
+        /**
+         * 邮件主题(标题)
+         */
+        subject?: string;
+        /**
+         * 邮件正文
+         */
+        text?: string;
+        /**
+         * 附件的路径。
+         */
+        attachment?: string;
+    }
+    /**
+     * 根据选项options调用邮箱应用发送邮件。如果没有安装邮箱应用，则抛出ActivityNotException。
+     */
+    function sendEmail(options: SendEmailOptions): void;
+    /**
+     * Intent(意图) 是一个消息传递对象，您可以使用它从其他应用组件请求操作。尽管 Intent 可以通过多种方式促进组件之间的通信.
+     */
+    interface Intent { }
+    /**
+     * 构造意图Intent对象所需设置。
+     */
+    interface IntentOptions {
+        action?: string;
+        type?: string;
+        data?: string;
+        category?: string[];
+        packageName?: string;
+        className?: string;
+        extras?: Object;
+    }
+    /**
+     * 根据选项，构造一个意图Intent对象。
+     */
+    function intent(options: IntentOptions): Intent;
+    /**
+     * 根据选项构造一个Intent，并启动该Activity。
+     */
+    function startActivity(intent: Intent): void;
+    /**
+     * 根据选项构造一个Intent，并发送该广播。
+     */
+    function sendBroadcast(intent: Intent): void;
 }
